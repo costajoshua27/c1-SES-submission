248f7a2924f8fe53258ae6194965d0bc0d35e3ba
Joshua Costa <costa.joshua27@gmail.com>
2019-11-02 12:21:02 -0700

    added api handler

--- flaskdriver/main/routes.py
+++ flaskdriver/main/routes.py
@@ -1,3 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
+from jService.api_handler import jService
 
 main = Blueprint("main", __name__)
+
+@main.route("/")
+@main.route("/home")
+def home():
+    jservice = jService()
+    url = jservice.build_search_url("random", count=5)
+    results = jservice.get_results(url)
+    return render_template("home.html", title="Home", results=results)
--- jService/api_handler.py
+++ jService/api_handler.py
@@ -1,1 +1,34 @@
-import requests
+import urllib.request
+import urllib.parse
+import re
+import json
+
+class jService:
+    def __init__(self):
+        self.base_url = "http://jservice.io/api"
+
+    def build_search_url(self, call_type: str, **kwargs) -> str:
+        parameters = [(key, value) for key, value in kwargs.items()]
+        url = self.base_url + f"/{call_type}"
+        return url + "?" + urllib.parse.urlencode(parameters) if parameters else url
+
+    def get_results(self, url: str) -> dict:
+        response = None
+
+        try:
+            response  = urllib.request.urlopen(url)
+            json_text = response.read().decode(encoding="utf-8")
+            return json.loads(json_text)
+
+        finally:
+            if response != None:
+                response.close()
+
+
+if __name__ == '__main__':
+    jservice = jService()
+    url = jservice.build_search_url("random", count=4)
+    print(url)
+    results = jservice.get_results(url)
+    for result in results:
+        print(result['question'], result['answer'], sep=":   ")
