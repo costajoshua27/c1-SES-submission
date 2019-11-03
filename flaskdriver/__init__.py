c08aa3938a712495666a759308f76eb3b76fce84
Joshua Costa <costa.joshua27@gmail.com>
2019-11-03 04:10:28 -0800

    revert changes

--- flaskdriver/__init__.py
+++ flaskdriver/__init__.py
@@ -3,11 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskdriver.config import Config
 
 
-def create_app(config_class = Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    from flaskdriver.main.routes import main
-    app.register_blueprint(main)
-
-    return app
+from flaskdriver.main.routes import main
+app.register_blueprint(main)
