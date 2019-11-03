0b4f5c5e7653e929191f367f2bd164261ee71c57
Joshua Costa <costa.joshua27@gmail.com>
2019-11-03 03:32:36 -0800

    removed db

--- flaskdriver/config.py
+++ flaskdriver/config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    SECRET_KEY = 'dfadfa' # os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
